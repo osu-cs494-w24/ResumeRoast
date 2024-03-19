b429ecdd4b9d82a4e528857f5fe0a8668bf0bd3c
michellehuyen <nguyemi4@oregonstate.edu>
2024-03-19 13:07:06 -0700

    changes?

--- resume-roast/src/pages/Login.jsx
+++ resume-roast/src/pages/Login.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import { useSearchParams } from 'react-router-dom'
+import { useCookies } from 'react-cookie'
 
 import LoginLink from '../components/LoginLink'
 
@@ -7,32 +8,35 @@ export default function Login() {
     const [ error, setError ] = useState("")
     const [ searchParams, setSearchParams ] = useSearchParams()
     const [ success, setSuccess ] = useState(false)
+    const [ cookies, setCookie ] = useCookies(["authCode"])
     const authCode = searchParams.get("code")
     
-    console.log("== authCode:", authCode)
     useEffect(() => {
         async function exchangeForAccessToken(code) {
-        const res = await fetch("http://localhost:8000/api/tokenExchange", {
-            method: "POST",
-            body: JSON.stringify({ code }),
-            headers: {
-                "Content-Type": "application/json"
+            const res = await fetch("/api/tokenExchange", {
+                method: "POST",
+                body: JSON.stringify({ code }),
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            })
+            if (res.status !== 200) {
+                setError("Error exchanging code for token")
+            } else {
+                let expires = new Date()
+                expires.setTime(expires.getTime() + (14400000)) // Token lasts 4 hours
+                setCookie('authCode', authCode, {path: '/', expires})
+                setSuccess(true)
             }
-        })
-        if (res.status !== 200) {
-            setError("Error exchanging code for token")
-        } else {
-            setSuccess(true)
-        }
         }
         if (authCode) {
             exchangeForAccessToken(authCode)
         }
-    }, [ authCode ])
+    }, [authCode, setCookie])
 
     return (
         <div>
-            {error && <p>Error: {error}</p>}
+            {error && !success && <p>Error: {error}</p>}
             {success ? <p>Success!</p> : <LoginLink />}
         </div>
     )
