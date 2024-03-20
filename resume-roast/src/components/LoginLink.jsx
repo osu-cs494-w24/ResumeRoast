447d0605cdc123cf7a685757c623758c1625166a
michellehuyen <nguyemi4@oregonstate.edu>
2024-03-19 19:34:37 -0700

    changed login link ui

--- resume-roast/src/components/LoginLink.jsx
+++ resume-roast/src/components/LoginLink.jsx
@@ -1,5 +1,3 @@
-import TopBar from '../pages/TopBar'
-
 import {NavLink} from 'react-router-dom'
 
 export default function LoginLink() {
@@ -15,13 +13,17 @@ export default function LoginLink() {
 
   return (
     <>
-    <button className="homepage" onClick={() => console.log('Button clicked!')}>
-    <NavLink to = "/" className = "homelink"> <img src="https://cdn-icons-png.freepik.com/256/10263/10263239.png" alt="Homepage"/> </NavLink>
-    </button>
+      <button className="homepage" onClick={() => console.log('Button clicked!')}>
+        <NavLink to = "/" className = "homelink"> <img src="https://cdn-icons-png.freepik.com/256/10263/10263239.png" alt="Homepage"/> </NavLink>
+      </button>
 
-    <h1> Resume Roasts! </h1>    
-    
-    <a href={url}>Login with Dropbox</a>
+      <div className="loginWrapper">
+        <div className="loginCard">
+          <h1>Welcome to Resume Roasts!</h1>    
+          
+          <a href={url}>Login with Dropbox</a>
+        </div>
+      </div>
     </>
   )
 }
