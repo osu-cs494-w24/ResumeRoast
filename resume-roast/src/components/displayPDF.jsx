37578ab9383040692fd11b3f3c12e5992788f947
Danny Nguyen <nguyeda3@gmail.com>
2024-03-19 23:47:36 -0700

    Add mobile responsiveness

--- resume-roast/src/components/displayPDF.jsx
+++ resume-roast/src/components/displayPDF.jsx
@@ -6,7 +6,7 @@ export default function DisplayPDF(props){
     const pdfLink  = "https://docs.google.com/viewer?url=" + pdfURL.pdfURL + "&embedded=true"
     return(
         <div>
-            <iframe src={pdfLink} width="900px" height="800px"></iframe>
+            <iframe src={pdfLink} width="850px" height="1100px"></iframe>
         </div>
     )
 }
